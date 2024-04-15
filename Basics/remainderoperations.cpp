f69d2b6ef6f6f5a5338020a864c26baa8d042626
Shruti Verma <vshruti186@gmail.com>
2024-04-15 23:43:54 +0530

    Basicp

---
+++ Basics/remainderoperations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<5%2<<endl;
+    cout<<5%5<<endl;
+    cout<<2%5<<endl;
+    cout<<5%(-2)<<endl;
+    cout<<(-5)%2<<endl;
+    cout<<(-5)%(-2)<<endl;
+
+}
