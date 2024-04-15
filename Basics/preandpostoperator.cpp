b5fd7d5dc219b7e941da12690c1b90da7f9845c1
Shruti Verma <vshruti186@gmail.com>
2024-04-15 23:46:59 +0530

    Basic programs-2

---
+++ Basics/preandpostoperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int x = 5;
+    // int y = ++x;
+    // cout<<x<<endl;
+    // cout<<y<<endl;
+    int x = 5;
+    int y = x++;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    return 0;
+}
